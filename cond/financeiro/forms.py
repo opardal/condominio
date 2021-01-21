e46c4c375647b8107635649b483aa7a964889cb0
joao <js.cad@pm.me>
2021-01-21 15:04:17 -0300

    Forms despesas finalizados

--- cond/financeiro/forms.py
+++ cond/financeiro/forms.py
@@ -1,7 +1,19 @@
 from django import forms
-from .models import Competencia
+from .models import Competencia, Despesa
 
 
 class CompetenciaForm(forms.Form):
     competencia = forms.IntegerField(widget=forms.Select(attrs = {'onchange' : "this.form.submit()"},
                                                          choices=[(c.id, c) for c in Competencia.objects.all()]))
+
+
+class DespesaForm(forms.ModelForm):
+
+    class Meta:
+        model = Despesa
+        fields = ['nome', 'valor', 'competencia']
+
+    def __init__(self, *args, **kwargs):
+        super(DespesaForm, self).__init__(*args, **kwargs)
+        if kwargs['initial']['is_comp']:
+            self.fields['competencia'].widget = forms.HiddenInput()
--- cond/financeiro/views.py
+++ cond/financeiro/views.py
@@ -1,30 +1,35 @@
 from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Despesa, Boleto, Competencia
-from .forms import CompetenciaForm
+from .forms import CompetenciaForm, DespesaForm
 
 
 class Despesas(ListView):
-    model = Despesa
     context_object_name = 'lista_de_despesas'
     template_name = 'financeiro/despesas.html'
-
-
-class DespesasComp(ListView):
-    #model = Despesa
-    context_object_name = 'lista_de_despesas'
-    template_name = 'financeiro/despesas_comp.html'
-
-    def get_queryset(self):
-        self.competencia = get_object_or_404(Competencia, id=self.kwargs['competencia'])
-        return Despesa.objects.filter(competencia=self.competencia)
     
+    def get_queryset(self):
+
+        try:
+            competencia = self.kwargs['competencia']
+            ano = competencia[:4]
+            mes = competencia[4:]
+            comp = Competencia.objects.get(ano=ano, mes=mes)
+            query_set = Despesa.objects.filter(competencia=comp)
+        except KeyError:
+            query_set = Despesa.objects.all()
+        return query_set
+
     def get_context_data(self):
         context = super().get_context_data()
-        context['competencia'] = self.competencia
+        try:
+            context['competencia'] = self.kwargs['competencia']
+            context['exibir_por_competencia'] = True
+        except KeyError:
+            context['exibir_por_competencia'] = False
         return context
 
 
@@ -32,10 +37,53 @@ class DespesaDetalhe(DetailView):
     model = Despesa
     template_name = 'financeiro/despesa_detail.html'
 
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data()
+
+        try:
+            context['competencia'] = self.kwargs['competencia']
+            context['exibir_por_competencia'] = True
+        except KeyError:
+            context['exibir_por_competencia'] = False
+
+        return context
+
 
 class DespesaCreate(CreateView):
-    model = Despesa
-    fields = ['nome', 'valor', 'competencia']
+    form_class = DespesaForm
+    template_name = 'financeiro/despesa_form.html'
+
+    def get_success_url(self):
+        try:
+            success_url = reverse('financeiro:despesas-comp', kwargs={'competencia': self.kwargs['competencia']})
+        except KeyError:
+            success_url = reverse('financeiro:despesas')
+        return success_url
+
+    def get_initial(self):
+
+        try:
+            competencia = self.kwargs['competencia']
+            ano = competencia[:4]
+            mes = competencia[4:] 
+            comp = Competencia.objects.get(ano=ano, mes=mes)
+            initial = {'competencia': comp.id, 'is_comp': True}
+        except KeyError:
+            comp = Competencia.objects.last()
+            initial = {'competencia': comp.id, 'is_comp': False}
+        return initial
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data()
+
+        try:
+            context['competencia'] = self.kwargs['competencia']
+            context['exibir_por_competencia'] = True
+        except KeyError:
+            context['exibir_por_competencia'] = False
+        return context
 
 
 class DespesaUpdate(UpdateView):
@@ -62,14 +110,15 @@ class FinanceiroIndex(TemplateView):
         context['competencia_selecionada'] = 1
         return context
     
-    def get (self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         context = self.get_context_data()
         context['form'] = CompetenciaForm(initial={'competencia':context['competencia_selecionada']})
+        context['competencia_str'] = Competencia.objects.get(id=context['competencia_selecionada'])
         return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
         context = self.get_context_data()
         context['competencia_selecionada'] = request.POST['competencia']
         context['form'] = CompetenciaForm(initial={'competencia':context['competencia_selecionada']})
+        context['competencia_str'] = Competencia.objects.get(id=context['competencia_selecionada'])
         return render(request, self.template_name, context)
-
